08fb76150194ac0807ff858d3e0d15f446447fa3
guigasauro <gui.ns7431@gmail.com>
2023-07-20 21:11:33 -0300

    mudando valores do exercício 1

--- coding_session1.py
+++ coding_session1.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import bisect
 
 def f(x):
-    return x**3 - 3.23*x**2 - 5.54*x + 9.84
+    return x**3 - x**2 - 6.54*x + 11.98
 
 x = np.linspace(-4, 5)
 plt.plot(x, f(x))
